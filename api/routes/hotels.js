12cf706a3ce7d33f6b68ae0bb5618150a3c6401e
MALARVIZHI M <Malarm2004@gmail.com>
2024-01-19 14:28:43 +0530

    Put and Post Hotels

---
+++ api/routes/hotels.js
@@ -0,0 +1,29 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+
+    const newHotel = new Hotel(req.body);
+
+    try {
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel);
+    } catch (err) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+router.put("/:id", async (req, res) => {
+
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id , {$set: req.body},{new:true})
+        res.status(200).json(updatedHotel);
+    } catch (err) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+
+export default router;
