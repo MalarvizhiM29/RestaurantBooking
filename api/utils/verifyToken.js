7cedfa10ed9e26198e8e5a84afc5520ce8c88dee
Varshini11113 <varshini.m2021csec@sece.ac.in>
2024-01-21 13:16:19 +0530

    All requests added & verified

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -4,23 +4,18 @@ import { createError } from "../utils/error.js";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
-    console.error("No token found");
     return next(createError(401, "You are not authenticated!"));
   }
 
   jwt.verify(token, process.env.JWT, (err, user) => {
-    if (err) {
-      console.error("Token verification error:", err);
-      return next(createError(403, "Token is not valid!"));
-    }
-
+    if (err) return next(createError(403, "Token is not valid!"));
     req.user = user;
     next();
   });
 };
 
 export const verifyUser = (req, res, next) => {
-  verifyToken(req, res, () => {
+  verifyToken(req, res, next, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
     } else {
@@ -30,7 +25,7 @@ export const verifyUser = (req, res, next) => {
 };
 
 export const verifyAdmin = (req, res, next) => {
-  verifyToken(req, res, () => {
+  verifyToken(req, res, next, () => {
     if (req.user.isAdmin) {
       next();
     } else {
