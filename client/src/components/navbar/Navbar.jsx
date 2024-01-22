80ab16529b70c4fe2eac70869e3e941dc962200c
MALARVIZHI M <Malarm2004@gmail.com>
2024-01-22 11:35:44 +0530

    Client and server completed

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,20 +1,50 @@
-import "./navbar.css"
-import { Link } from "react-router-dom"
+import "./navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  const handleRegisterClick = () => {
+    navigate("/register");
+  };
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
-        <span className="logo">UVMbooking</span>
+          <span className="logo">UVMbooking</span>
         </Link>
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        {user ? (
+          <div className="navItems">
+          <span>{capitalizeFirstLetter(user.username)}</span>
+            <button className="navButton">
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div className="navItems">
+            <button className="navButton" onClick={handleRegisterClick}>
+              Register
+            </button>
+            <button className="navButton" onClick={handleLoginClick}>
+              Login
+            </button>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
